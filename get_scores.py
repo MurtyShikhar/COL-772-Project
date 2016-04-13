55a462b3d31562f6842710ff0d96ec4ccb84c966
Shikhar <apple@Apples-MacBook-Pro.local>
2016-04-13 12:23:29 +0530

    Put stuff into a class

--- get_scores.py
+++ get_scores.py
@@ -11,58 +11,58 @@ import gzip
 from scipy import spatial
 import sys
 import time
-#fin = gzip.open('/Users/apple/Desktop/Spring-2016/NLP_IITD/jeevan_shankar-multi-sense-skipgram-74aeafd22528/vectors-KMeans-neg15-MSSG.gz')
 fin = gzip.open('/Users/apple/Desktop/Spring-2016/NLP_IITD/release/vectors.MSSG.300D.30K.gz')
 lines = fin.readlines()
 fin.close()
 
-#V, D, S, maxoutMethod
-V, D = map(lambda i: int(i)  ,lines[0].strip().split(" "))
-S = 3
-maxoutMethod = 0
-print(' # words in vocab := %d, size := %d, senses = %d, maxoutMethod = %d' %(V, D, S, maxoutMethod))
-vocab = {}
-inverse_vocab = {}
-nCluster = {}
-weights = {}
-nClusterCount = {}
-nClusterCenter = {}
-j = 1
-for i in xrange(V):
-    line = lines[j].strip().split(" ")
-    j+=1
-    vocab[i] = line[0].lower()
-    inverse_vocab[vocab[i]] = i
-    
-    nCluster[i] = int(line[1]) if (len(line) > 1) else S
-    if (len(line) > 2):
-        nClusterCount[v] = []
-        for k in xrange(nCluster[i]):
-            nClusterCount[v].append( int(line(k+2)))
-    weights[i] = {}
-    nClusterCenter[i] = {}
-    weights[i][0] = np.zeros(D)
-    line = lines[j].strip().split(" ")
-    for d in xrange(D): weights[i][0][d] = float(line[d])
 
-    j+=1
-    for s in xrange(1, nCluster[i] + 1):
-        line = lines[j].strip().split(" ")
-        j+=1
-        weights[i][s] = np.zeros(D)
-        for d in xrange(D): weights[i][s][d] = float(line[d])
-        weights[i][s] /= np.linalg.norm(weights[i][s])
-        if (maxoutMethod == 0):
+class read_model:
+
+    def __init__(self ):
+        self.vocab = {}
+        self.inverse_vocab = {}
+        self.nCluster = {}
+        self.weights = {}
+        self.nClusterCount = {}
+        self.nClusterCenter = {}
+
+
+    def read_pretrained(self):
+        V, D = map(lambda i: int(i)  ,lines[0].strip().split(" "))
+        j = 1
+        S = 3
+        maxoutMethod = 0
+        print(' # words in vocab := %d, size := %d, senses = %d, maxoutMethod = %d' %(V, D, S, maxoutMethod))
+        for i in xrange(V):
             line = lines[j].strip().split(" ")
             j+=1
-            nClusterCenter[i][s] = np.zeros(D)
-            for d in xrange(D): nClusterCenter[i][s][d] = float(line[d])
+            self.vocab[i] = line[0].lower()
+            self.inverse_vocab[self.vocab[i]] = i
+        
+            self.nCluster[i] = int(line[1]) if (len(line) > 1) else S
+            if (len(line) > 2):
+                self.nClusterCount[i] = []
+                for k in xrange(self.nCluster[i]):
+                    self.nClusterCount[i].append( int(line(k+2)))
+            self.weights[i] = {}
+            self.nClusterCenter[i] = {}
+            self.weights[i][0] = np.zeros(D)
+            line = lines[j].strip().split(" ")
+            for d in xrange(D): self.weights[i][0][d] = float(line[d])
 
-
-    print '\r>> Done with %dth iteration' %i,
-    sys.stdout.flush()
-    #time.sleep(0.1)
-fin.close()
+            j+=1
+            for s in xrange(1, self.nCluster[i] + 1):
+                line = lines[j].strip().split(" ")
+                j+=1
+                self.weights[i][s] = np.zeros(D)
+                for d in xrange(D): self.weights[i][s][d] = float(line[d])
+                self.weights[i][s] /= np.linalg.norm(self.weights[i][s])
+                if (maxoutMethod == 0):
+                    line = lines[j].strip().split(" ")
+                    j+=1
+                    self.nClusterCenter[i][s] = np.zeros(D)
+        print '\r>> Done with %dth iteration' %i,
+        sys.stdout.flush()
 
 
 dataset = "/Users/apple/Desktop/Spring-2016/NLP_IITD/Project/multi-sense/MSSG/resources/ratings.txt"
@@ -80,10 +80,14 @@ print(len(words))
 f.close()
 
 average_scores = np.array(average_scores)
+Model = read_model() 
+Model.read_pretrained()
+
+
 
 def getID(w1):
    # print w1
-    return -1 if w1 not in inverse_vocab else inverse_vocab[w1] 
+    return -1 if w1 not in Model.inverse_vocab else Model.inverse_vocab[w1] 
 
 
 
@@ -92,12 +96,12 @@ def get_embedding_context_all(id, cont):
     j =map(lambda i: getID(i), l)
     contextIDs = filter(lambda i : i != -1, j)
     senseContext = getSense(id, contextIDs)        
-    return weights[id][senseContext] if maxoutMethod != 0 else nClusterCenter[id][senseContext]
+    return Model.weights[id][senseContext] if maxoutMethod != 0 else Model.nClusterCenter[id][senseContext]
 
 def get_embedding_context(id, cont):
 
     l = map(lambda i: i.lower().strip(), cont.split(" "))
-    word = vocab[id]
+    word = Model.vocab[id]
 
     for i in xrange(len(l)):
         if l[i] == word:
@@ -125,19 +129,11 @@ def get_embedding_context(id, cont):
             p+=1
         j-=1
     
-    print vocab[id],
-
-    for i in contextIDs:
-        print vocab[i],
-        print " ",
-
-    print("\n")    
     senseContext = getSense(id, contextIDs)
-    #print(len(contextIDs))
-    return weights[id][senseContext] if maxoutMethod != 0 else nClusterCenter[id][senseContext]
+    return Model.weights[id][senseContext] if maxoutMethod != 0 else Model.nClusterCenter[id][senseContext]
 
 def prob(id, contextEmbedding, currSense):
-    currEmbedding = weights[id][currSense]
+    currEmbedding = Model.weights[id][currSense]
     return 1.0- spatial.distance.cosine(contextEmbedding, currEmbedding)
 
 
@@ -150,10 +146,10 @@ def avgSimC(w1, w2, c):
     score = 0.0
     contextEmbedding_w1 = get_embedding_context(id1, context[c][0]) if opt == 1 else get_embedding_context_all(id1, context[c][0])
     contextEmbedding_w2 = get_embedding_context(id2, context[c][1]) if opt == 1 else get_embedding_context_all(id2, context[c][1])
-    for i in xrange(1, nCluster[id1]+1):
-        for j in xrange(1,nCluster[id2]+1):
-            embedding_w1 = weights[id1][i]
-            embedding_w2 = weights[id2][j]
+    for i in xrange(1, Model.nCluster[id1]+1):
+        for j in xrange(1,Model.nCluster[id2]+1):
+            embedding_w1 = Model.weights[id1][i]
+            embedding_w2 = Model.weights[id2][j]
             score_i = 1.0 -spatial.distance.cosine(embedding_w1, embedding_w2)
             #print(score_i)
             score+= score_i*prob(id1, contextEmbedding_w1, i)*prob(id2, contextEmbedding_w2, j) 
@@ -166,31 +162,31 @@ def avgSim(w1, w2):
         return 0
     
     score = 0.0
-    for i in xrange(1, nCluster[id1] +1):
-        for j in xrange(1, nCluster[id2] + 1):
-            embedding_w1 = weights[id1][i]
-            embedding_w2 = weights[id2][j]
+    for i in xrange(1, Model.nCluster[id1] +1):
+        for j in xrange(1, Model.nCluster[id2] + 1):
+            embedding_w1 = Model.weights[id1][i]
+            embedding_w2 = Model.weights[id2][j]
             score += 1.0 - spatial.distance.cosine(embedding_w1, embedding_w2)
-    return score/(nCluster[id1]*nCluster[id2])        
+    return score/(Model.nCluster[id1]*Model.nCluster[id2])        
 
 
 def global_sim(w1, w2):
     id1 = getID(w1); id2 = getID(w2);
     if (id1 == -1 or id2 == -1): return -1
 
-    embedding_w1 = weights[id1][0]
-    embedding_w2 = weights[id2][0]
+    embedding_w1 = Model.weights[id1][0]
+    embedding_w2 = Model.weights[id2][0]
     return 1.0 - spatial.distance.cosine(embedding_w1, embedding_w2)
 
 def getSense(wordId, contextIDs):
     contextEmbedding = np.zeros(D)
     for i in xrange(len(contextIDs)):
-       contextEmbedding += weights[contextIDs[i]][0]
+       contextEmbedding += Model.weights[contextIDs[i]][0]
 
     correctSense = 0
     max_score = -1*float('inf')
-    for i in xrange(1, nCluster[wordId]+1):
-        score = np.dot(contextEmbedding, weights[wordId][i])
+    for i in xrange(1, Model.nCluster[wordId]+1):
+        score = np.dot(contextEmbedding, Model.weights[wordId][i])
         if (score > max_score): 
             max_score = score
             correctSense = i
